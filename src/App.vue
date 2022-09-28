858876cf00ac59fe1ac95d2c68f69e1a18846b35
Даниэль Давлетшин <Superdanik2004@gmail.com>
2022-09-28 19:03:53 +0500

    Some prerenderer config changes

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="q-app">
+    <div class="bg"></div>
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style lang="scss" scoped>
+#q-app {
+  position: relative;
+
+  & > .bg {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #dcc589;
+  }
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-page class="main">
+    <section class="column text-white items-center products section">
+      <div class="col q-px-lg justify-center container q-gutter-y-lg products_content"></div>
+    </section>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+});
+</script>
+
+<style lang="scss" scoped>
+.products {
+  &_content {
+    display: flex;
+    flex-wrap: wrap;
+    grid-template-columns: auto;
+    column-gap: 64px;
+
+    > .flex {
+      flex-direction: column;
+      row-gap: 32px;
+    }
+
+    @media (min-width: $breakpoint-sm-max) {
+      grid-template-columns: auto auto;
+    }
+
+    @media (min-width: $breakpoint-md-max) {
+      grid-template-columns: auto auto auto;
+    }
+  }
+}
+
+.about {
+  padding: 16px;
+}
+
+.description {
+  grid-row: span 4;
+  grid-column: 2;
+  color: $text-color;
+}
+</style>
